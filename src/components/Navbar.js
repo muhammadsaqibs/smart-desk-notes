1a492e445cfb4fb8f476667828020c98e239ca7c
muhammad saqib <saqib.s09651100@gmail.com>
2025-07-18 06:30:12 +0500

    title changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav style={{ padding: '1rem', backgroundColor: '#282c34', color: 'white' }}>
-      <h2><Link to="/" style={{ color: 'white', textDecoration: 'none' }}>SmartDesk Notes App</Link></h2>
+      <h2><Link to="/" style={{ color: 'white', textDecoration: 'none' }}>SmartDesk Notes App For Students</Link></h2>
     </nav>
   );
 }
